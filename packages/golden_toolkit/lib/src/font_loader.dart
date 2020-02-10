8567a776af421d02e283d28751e700132f178b74
Corey Sprague <cosprague@ebay.com>
2020-02-10 13:53:50 -0800

    embed roboto/materialicons in golden_toolkit (since unavailable from flutter directly), removed folder injection

--- packages/golden_toolkit/lib/src/font_loader.dart
+++ packages/golden_toolkit/lib/src/font_loader.dart
@@ -7,8 +7,6 @@ import 'dart:convert';
 /// license that can be found in the LICENSE file or at
 /// https://opensource.org/licenses/BSD-3-Clause
 /// ***************************************************
-import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -19,38 +17,6 @@ import 'package:flutter_test/flutter_test.dart';
 ///To make the goldens more useful, we have a utility to dynamically inject additional fonts into the flutter test engine so that we can get more human viewable output.
 /// Path to your folder with fonts [from] in required
 Future<void> loadAppFonts({Iterable<String> sourceDirectories}) async {
-  await _loadAllFontsFromPackages();
-  await _loadFontsFromDirectories(sourceDirectories);
-}
-
-Future _loadFontsFromDirectories(Iterable<String> sourceDirectories) async {
-  final Map<String, List<ByteData>> fontFamilies = {};
-  for (final from in ['../golden_toolkit/fonts']
-    ..followedBy(sourceDirectories ?? <String>[])) {
-    final fontsDir = Directory.fromUri(Uri(path: _getPath(from)));
-
-    await for (final entity in fontsDir.list()) {
-      if (entity.path.endsWith('.ttf')) {
-        final fontName =
-            Uri.parse(entity.path).pathSegments.last.split('.ttf').first;
-        final family = fontName.split('-').first;
-        final Uint8List bytes = await File.fromUri(entity.uri).readAsBytes();
-        final byteData = ByteData.view(bytes.buffer);
-        fontFamilies[family] =
-            [byteData].followedBy(fontFamilies[family] ?? []).toList();
-      }
-    }
-  }
-  for (final family in fontFamilies.keys) {
-    final loader = FontLoader(family);
-    for (final font in fontFamilies[family]) {
-      loader.addFont(Future.value(font));
-    }
-    await loader.load();
-  }
-}
-
-Future _loadAllFontsFromPackages() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   final fontManifest = await rootBundle.loadStructuredData<List<dynamic>>(
     'FontManifest.json',
@@ -63,15 +29,5 @@ Future _loadAllFontsFromPackages() async {
       fontLoader.addFont(rootBundle.load(fontType['asset']));
     }
     await fontLoader.load();
-  }
-}
-
-/// Relative pathing behaves differently when running all tests for a package vs. running a particular test file
-/// Without this, either the commandline or the IDE will fail when running tests.
-String _getPath(String directory) {
-  if (Directory.current.path.endsWith('test')) {
-    return '../$directory';
-  } else {
-    return directory;
   }
 }
