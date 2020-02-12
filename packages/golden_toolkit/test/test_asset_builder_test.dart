94b1bba2b342463b314c9c127605bf649558510d
Corey Sprague <cosprague@ebay.com>
2020-02-12 15:12:54 -0800

    chase the RIGHT coverage

--- packages/golden_toolkit/test/test_asset_builder_test.dart
+++ packages/golden_toolkit/test/test_asset_builder_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/src/test_asset_bundle.dart';
 
@@ -6,9 +7,14 @@ void main() {
   group('TestAssetBundle', () {
     /* chasing code coverage... we're at 99% coverage, and this slightly forked code is the last 1% */
     test('Should throw if asset not found', () {
-      final bundle = TestAssetBundle();
+      final bundle = FakeTestAssetBundle();
       expectLater(() => bundle.loadString('nothing'),
           throwsA(isInstanceOf<FlutterError>()));
     });
   });
+}
+
+class FakeTestAssetBundle extends TestAssetBundle {
+  @override
+  Future<ByteData> load(String key) async => null;
 }
