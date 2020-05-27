c0ec5f0114d298bacf16d65ea27831b682d264ee
Christian Mürtz <christian.muertz@wetteronline.de>
2020-05-27 21:21:03 +0200

    Make analyzer happy

--- packages/golden_toolkit/lib/src/multi_screen_golden.dart
+++ packages/golden_toolkit/lib/src/multi_screen_golden.dart
@@ -9,8 +9,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
 
+import '../golden_toolkit.dart';
 import 'device.dart';
 import 'testing_tools.dart';
 
