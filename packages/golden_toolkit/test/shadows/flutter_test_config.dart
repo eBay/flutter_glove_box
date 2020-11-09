90cb227a3bc0913f9a864050af719c6a08736c7c
Thibaud Desodt <thibaud.desodt@koahealth.com>
2020-11-09 20:52:22 +0100

    Introduce GoldenToolkitConfiguration.enableRealShadows
    By default, Widget tests render shadows as opaque/solid blocks. The goal is to make rendering more deterministic and avoid false negative when using Golden tests.
    
    When using the generated goldens as "living documentation" though, we do want those goldens to look as close as possible to reality.
    
    Now `enableRealShadows` allow to use "real shadows" in generated goldens. 
    
    This can be done globally by adding a global flag in a `flutter_test_config.dart` file
    
    ```dart
    Future<void> main(FutureOr<void> Function() testMain) async {
      return GoldenToolkit.runWithConfiguration(
        () async {
          await testMain();
        },
        config: GoldenToolkitConfiguration(
          enableRealShadows: true,
        ),
      );
    }
    ```
    
    or closer to the test (note that `GoldenToolkit.runWithConfiguration` must surround the `testGoldens()` call for it to work)
    
    ```dart
    GoldenToolkit.runWithConfiguration(() => {
          testGoldens(
              'screenMatchesGolden method can disable enableRealShadows temporarily',
              (tester) async {
            await tester.pumpWidgetBuilder(WidgetWithShadows());
            await screenMatchesGolden(
                tester, 'my_golden');
          })
        },
        config: GoldenToolkit.configuration.copyWith(enableRealShadows: false),
      );
    });
    ```
    
    fixes #72

---
+++ packages/golden_toolkit/test/shadows/flutter_test_config.dart
@@ -0,0 +1,29 @@
+/// ***************************************************
+/// Copyright 2019-2020 eBay Inc.
+///
+/// Use of this source code is governed by a BSD-style
+/// license that can be found in the LICENSE file or at
+/// https://opensource.org/licenses/BSD-3-Clause
+/// ***************************************************
+///
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> main(FutureOr<void> Function() testMain) async {
+  return GoldenToolkit.runWithConfiguration(
+    () async {
+      await testMain();
+    },
+    config: GoldenToolkitConfiguration(
+      // Currently, goldens are not generated/validated in CI for this repo. We have settled on the goldens for this package
+      // being captured/validated by developers running on MacOSX. We may revisit this in the future if there is a reason to invest
+      // in more sophistication
+      skipGoldenAssertion: () => !Platform.isMacOS,
+      // enableRealShadows can only work when "outside" of a `testGoldens` , for instance here at the root
+      enableRealShadows: true,
+    ),
+  );
+}
---
+++ packages/golden_toolkit/test/shadows/shadows_test.dart
@@ -0,0 +1,54 @@
+/// ***************************************************
+/// Copyright 2019-2020 eBay Inc.
+///
+/// Use of this source code is governed by a BSD-style
+/// license that can be found in the LICENSE file or at
+/// https://opensource.org/licenses/BSD-3-Clause
+/// ***************************************************
+///
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import '../sample_widgets.dart';
+
+void main() {
+  group('Shadows Tests', () {
+    setUp(() {
+      expect(debugDisableShadows, isTrue,
+          reason: 'debugDisableShadows should always be true before test');
+    });
+
+    tearDown(() {
+      expect(debugDisableShadows, isTrue,
+          reason: 'debugDisableShadows should always be true after test');
+    });
+
+    testGoldens(
+        'screenMatchesGolden method uses enableRealShadows from global configuration (flutter_test_config.dart)',
+        (tester) async {
+      await tester.pumpWidgetBuilder(WidgetWithShadows());
+      await screenMatchesGolden(tester, 'realShadows_enabled_globally');
+
+      expect(debugDisableShadows, isFalse,
+          reason: 'debugDisableShadows should be false during test');
+    });
+
+    // wraping needs to happen outside of testGoldens
+    GoldenToolkit.runWithConfiguration(
+      () => {
+        testGoldens(
+            'screenMatchesGolden method can disable enableRealShadows temporarily',
+            (tester) async {
+          await tester.pumpWidgetBuilder(WidgetWithShadows());
+          await screenMatchesGolden(tester, 'realShadows_disabled_locally');
+
+          expect(debugDisableShadows, isTrue,
+              reason: 'debugDisableShadows should be false during test');
+        })
+      },
+      config: GoldenToolkit.configuration.copyWith(enableRealShadows: false),
+    );
+  });
+}
