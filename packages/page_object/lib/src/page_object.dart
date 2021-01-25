26d873a7d9db8ec5a1ef101d4f5f8ee2f9f0aac6
Larry McKenzie <lmckenzie@ebay.com>
2021-01-25 10:00:11 -0800

    Formatting

--- packages/page_object/lib/src/page_object.dart
+++ packages/page_object/lib/src/page_object.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-/// This class exists so page objects can seamlessly be passed to widget testing APIs as Finders. This helps
-/// to remove code noise from tests. See the tests in this package for an example.
+/// This class exists so page objects can seamlessly be passed to widget testing
+/// APIs as Finders. This helps to remove code noise from tests. See the tests
+/// in this package for an example.
 class PageObject extends Finder {
   /// Creates a [PageObject] for this [Finder]
   PageObject(this._finder) : assert(_finder != null);
   final Finder _finder;
 
   @override
-  Iterable<Element> apply(Iterable<Element> candidates) => _finder.apply(candidates);
+  Iterable<Element> apply(Iterable<Element> candidates) =>
+      _finder.apply(candidates);
 
   @override
   String get description => _finder.description;
